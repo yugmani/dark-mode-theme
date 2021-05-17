ddd3cccc477944f5de19de65f95e810ab3a1151d
Himalaya <yugmani@yahoo.com>
2021-05-17 09:01:53 -0700

    Dark/light persistent: localstorage web api

--- main.js
+++ main.js
@@ -3,4 +3,15 @@ const switchButton = document.getElementById("switch");
 switchButton.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   switchButton.classList.toggle("active");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
 });
+
+if (localStorage.getItem("darkMode") == "enabled") {
+  document.body.classList.toggle("dark");
+  switchButton.classList.toggle("active");
+  // workContainer.classList.toggle("dark");
+}
